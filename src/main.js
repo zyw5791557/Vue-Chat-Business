c61d9d5a9fd6bad7b1166f538ee6311cf3b579af
Emlice <xiaomozyw@sina.com>
2018-02-09 15:24:02 +0800

    update dev install

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ import config from './config';
 import io from 'socket.io-client';
 
 // 连接到远程socket地址。
-// const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'http://localhost:3000';
 const socket = io.connect(SOCKET_URL, {
 	forceNew: true,
 	reconnection : true
