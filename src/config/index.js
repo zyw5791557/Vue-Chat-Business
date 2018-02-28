129cc947bce5917019bf0e7878f8b09c5e99ce3d
Emlice <xiaomozyw@sina.com>
2018-02-28 10:45:53 +0800

    配置服务器 socket-io 地址

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    $SOCKET_URL: 'http://localhost:3000',           // socket 服务地址
+    // $SOCKET_URL: 'http://localhost:3000',           // socket 服务地址 | 线下开发
+    $SOCKET_URL: 'http://chat.emlice.top',           // socket 服务地址 | 服务器
     $STATIC_URL: 'http://localhost:8989',           // 资源服务器地址
 }
